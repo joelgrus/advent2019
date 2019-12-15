f330c0a7fd999754c33ee0dd91453fd88a99b025
Joel Grus <joelgrus@gmail.com>
2019-12-15 06:45:16 -0800

    day 15

--- day15/day15.py
+++ day15/day15.py
@@ -1,3 +1,4 @@
+
 """
 After solving this problem I went back and refactored this code again,
 under the assumption that I'd probably have to use it again on a later day.
@@ -50,21 +51,15 @@ def parse_opcode(opcode: int, num_modes: int = 3) -> Tuple[Opcode, Modes]:
 
 class IntcodeComputer:
     def __init__(self, program: List[int], get_input: Callable[[], int]) -> None:
-        self.program = program[:]
+        self.program = defaultdict(int)
+        self.program.update({i: value for i, value in enumerate(program)})
         self.get_input = get_input
         self.pos = 0
         self.relative_base = 0
 
-    def _expand(self, pos: int) -> None:
-        while len(self.program) <= pos:
-            self.program.append(0)
-
     def _get_value(self, pos: int, mode: int) -> int:
-        self._expand(pos)
-
         if mode == 0:
             # pointer mode
-            self._expand(self.program[pos])
             # logging.debug(f"pos: {pos}, mode: {mode}, value: {self.program[self.program[pos]]}")
             return self.program[self.program[pos]]
         elif mode == 1:
@@ -73,28 +68,22 @@ class IntcodeComputer:
             return self.program[pos]
         elif mode == 2:
             # relative mode
-            self._expand(self.program[pos] + self.relative_base)
             # logging.debug(f"pos: {pos}, mode: {mode}, value: {self.program[self.program[pos] + self.relative_base]}")
             return self.program[self.program[pos] + self.relative_base]
         else:
             raise ValueError(f"unknown mode: {mode}")
 
     def _loc(self, pos: int, mode: int) -> int:
-        self._expand(pos)
-
         if mode == 0:
             # pointer mode
-            self._expand(self.program[pos])
             # logging.debug(f"pos: {pos}, mode: {mode}, value: {self.program[pos]}")
             return self.program[pos]
         elif mode == 2:
             # relative mode
-            self._expand(self.program[pos] + self.relative_base)
             # logging.debug(f"pos: {pos}, mode: {mode}, value: {self.program[pos] + self.relative_base}")
             return self.program[pos] + self.relative_base
 
-    def __call__(self, input_values: Iterable[int] = ()) -> int:
-        self.inputs.extend(input_values)
+    def go(self) -> int:
 
         while True:
             # logging.debug(f"program: {self.program}")
@@ -130,7 +119,7 @@ class IntcodeComputer:
             elif opcode == Opcode.STORE_INPUT:
                 # Get input and store at location
                 loc = self._loc(self.pos + 1, modes[0])
-                input_value = self.inputs.popleft()
+                input_value = self.get_input()
                 self.program[loc] = input_value
                 self.pos += 2
 
@@ -141,7 +130,6 @@ class IntcodeComputer:
                 # logging.debug(f"output: {value}")
 
                 ####
-                self.inputs.clear()
                 ####
 
                 return value
@@ -206,117 +194,73 @@ class IntcodeComputer:
             else:
                 raise ValueError(f"invalid opcode: {opcode}")
 
-class Tile(Enum):
-    EMPTY = 0
-    WALL = 1
-    BLOCK = 2
-    PADDLE = 3
-    BALL = 4
+
+PROGRAM = [3,1033,1008,1033,1,1032,1005,1032,31,1008,1033,2,1032,1005,1032,58,1008,1033,3,1032,1005,1032,81,1008,1033,4,1032,1005,1032,104,99,101,0,1034,1039,1002,1036,1,1041,1001,1035,-1,1040,1008,1038,0,1043,102,-1,1043,1032,1,1037,1032,1042,1105,1,124,1001,1034,0,1039,101,0,1036,1041,1001,1035,1,1040,1008,1038,0,1043,1,1037,1038,1042,1105,1,124,1001,1034,-1,1039,1008,1036,0,1041,102,1,1035,1040,1001,1038,0,1043,101,0,1037,1042,1106,0,124,1001,1034,1,1039,1008,1036,0,1041,101,0,1035,1040,1002,1038,1,1043,101,0,1037,1042,1006,1039,217,1006,1040,217,1008,1039,40,1032,1005,1032,217,1008,1040,40,1032,1005,1032,217,1008,1039,33,1032,1006,1032,165,1008,1040,35,1032,1006,1032,165,1101,0,2,1044,1105,1,224,2,1041,1043,1032,1006,1032,179,1101,0,1,1044,1106,0,224,1,1041,1043,1032,1006,1032,217,1,1042,1043,1032,1001,1032,-1,1032,1002,1032,39,1032,1,1032,1039,1032,101,-1,1032,1032,101,252,1032,211,1007,0,68,1044,1105,1,224,1101,0,0,1044,1106,0,224,1006,1044,247,101,0,1039,1034,102,1,1040,1035,1001,1041,0,1036,102,1,1043,1038,101,0,1042,1037,4,1044,1105,1,0,30,84,39,21,27,93,20,65,45,95,19,6,71,25,33,13,80,53,60,70,65,80,45,65,53,62,93,13,19,72,33,49,54,92,9,29,25,69,7,46,9,96,97,70,8,69,71,97,3,75,94,49,96,11,76,24,29,84,87,99,33,76,83,83,21,62,97,82,63,71,78,74,29,94,90,34,92,58,75,44,66,99,28,37,84,18,18,94,86,50,4,74,3,96,74,39,99,55,93,44,94,55,40,78,2,88,70,6,69,67,87,40,4,93,76,30,1,42,40,87,23,83,89,24,73,19,62,88,43,92,94,50,71,53,19,75,22,9,82,46,65,84,92,63,99,57,23,62,93,61,14,87,67,84,90,38,96,83,33,63,40,80,75,10,79,89,52,14,97,32,87,72,57,79,7,79,6,93,66,77,50,19,97,78,65,96,24,94,80,12,10,70,9,60,77,67,17,83,76,36,79,27,43,91,6,72,77,49,4,47,56,85,81,11,46,96,93,33,82,44,69,49,34,98,77,95,38,19,85,1,62,73,49,95,39,62,36,83,23,93,34,32,21,94,89,30,85,76,13,92,87,3,84,43,3,74,39,81,6,85,16,69,89,21,56,80,65,92,84,97,7,63,23,8,87,37,70,54,75,92,95,96,51,83,34,24,86,39,59,48,89,45,34,89,72,3,77,63,98,38,70,39,38,98,97,85,46,96,53,81,89,27,83,75,31,81,71,39,81,62,79,11,78,18,90,94,1,91,1,79,77,74,64,20,73,55,75,78,2,77,24,92,56,55,25,70,21,38,69,49,81,19,34,92,97,74,61,79,18,77,51,76,62,92,10,85,83,87,39,90,31,98,95,61,32,63,82,59,75,65,53,72,91,17,75,75,54,85,57,32,13,39,70,48,86,59,50,96,32,23,84,61,85,48,59,92,33,15,58,83,95,48,80,70,84,58,69,70,37,99,18,73,79,32,71,22,41,75,26,71,25,55,73,31,5,53,71,95,65,87,50,62,95,80,54,95,73,79,20,94,65,83,33,26,88,3,11,99,76,93,28,97,67,49,90,94,19,85,28,10,96,70,55,84,17,75,33,47,91,44,88,96,1,6,89,40,69,27,58,98,61,25,77,79,43,83,38,13,72,44,99,20,33,69,8,5,47,72,78,24,53,94,78,39,99,87,9,63,82,52,69,64,48,93,46,48,89,22,84,32,69,7,36,99,80,4,27,92,54,14,85,56,19,99,93,99,49,67,82,90,23,10,77,77,37,79,67,78,27,81,79,34,67,81,40,88,76,89,94,64,80,73,79,57,72,22,14,93,3,88,84,88,41,12,29,4,97,57,83,38,93,51,55,20,75,57,78,22,76,22,24,85,91,79,27,19,46,90,18,71,3,39,28,26,94,87,83,31,35,73,56,99,83,35,65,92,45,98,93,2,73,88,15,90,62,85,95,20,96,75,52,4,62,81,78,49,67,69,20,5,85,72,79,45,34,73,89,20,37,60,79,97,6,41,78,40,70,42,29,89,21,76,88,44,82,17,9,73,52,71,73,25,89,71,30,82,85,26,86,61,43,7,71,13,99,72,40,95,79,39,67,39,65,90,91,14,96,20,73,98,66,13,92,70,1,93,2,86,45,54,85,73,30,62,14,97,89,39,77,99,40,89,76,49,97,42,60,97,62,82,35,98,49,80,15,91,34,87,65,77,44,93,65,87,76,82,20,78,46,90,18,81,73,98,47,99,48,69,2,82,90,90,47,85,49,94,37,81,76,90,0,0,21,21,1,10,1,0,0,0,0,0,0]
 
 
-with open('day13.txt') as f:
-    program = [int(n) for n in f.read().strip().split(",")]
+class Direction(Enum):
+    NORTH = 1
+    SOUTH = 2
+    WEST = 3
+    EAST = 4
+
+class Status(Enum):
+    WALL = 0
+    MOVED = 1
+    OXYGEN = 2
+
+DIRECTIONS = [Direction.NORTH, Direction.SOUTH, Direction.WEST, Direction.EAST]
+ANTI_DIRECTIONS = [Direction.SOUTH, Direction.NORTH, Direction.EAST, Direction.WEST]
+DELTAS = [(0, 1), (0, -1), (-1, 0), (1, 0)]
 
 
-def count_blocks(program: Program) -> int:
-    screen: Dict[Tuple[int, int], Tile] = {}
-    computer = IntcodeComputer(program)
+def find_oxygen() -> int:
+    """
+    return the length of the shortest path to the oxygen
+    """
+    visited = {(0, 0)}
 
-    try:
-        while True:
-            x = computer()
-            y = computer()
-            tile = Tile(computer())
-            screen[(x, y)] = tile
-    except EndProgram:
-        # return len([tile for tile in screen.values() if tile == Tile.BLOCK])
-        return screen
-# print(count_blocks(program))
+    frontier = deque([])
+    frontier.append(([], (0, 0)))
 
-# screen = count_blocks(program)
+    while frontier:
+        inputs = deque([])
+        def get_input() -> int:
+            return inputs.popleft()
 
-def show(tile: Tile) -> str:
-    if tile == Tile.BALL:
-        return 'o'
-    elif tile == Tile.BLOCK:
-        return '#'
-    elif tile == Tile.EMPTY:
-        return ' '
-    elif tile == Tile.PADDLE:
-        return '-'
-    elif tile == Tile.WALL:
-        return 'X'
-    else:
-        raise RuntimeError(f"bad tile: {tile}")
+        computer = IntcodeComputer(PROGRAM, get_input)
+        path, (x, y) = frontier.popleft()
 
-def draw(screen: Dict[Tuple[int, int], Tile]) -> None:
-    if not screen:
-        return
-    x_min = min(x for x, y in screen)
-    x_max = max(x for x, y in screen)
-    y_min = min(y for x, y in screen)
-    y_max = max(y for x, y in screen)
+        print(x, y, len(path))
 
-    for y in range(y_min, y_max + 1):
-        row = [show(screen.get((x, y), Tile.EMPTY)) for x in range(x_min, x_max + 1)]
-        print("".join(row))
+        for i in path:
+            inputs.append(i)
+            computer.go()
+
+        for direction, anti, (dx, dy) in zip(DIRECTIONS, ANTI_DIRECTIONS, DELTAS):
+            #print(direction, anti, dx, dy)
+            inputs.append(direction.value)
+            status = Status(computer.go())
+            #print("status", status)
+            if status == Status.OXYGEN:
+                return len(path) + 1
+            elif status == Status.WALL:
+                pass
+            elif status == Status.MOVED:
+                new_loc = (x + dx, y + dy)
+                #print(new_loc, visited)
+                if new_loc not in visited:
+                    new_path = path + [direction.value]
+                    # print("new location", new_loc, new_path)
+                    visited.add(new_loc)
+                    frontier.append((new_path, new_loc))
+                    # move back
+                    inputs.append(anti.value)
+                    computer.go()
+                else:
+                    #print("been here")
+                    pass
+            
+print(find_oxygen())
 
 
-def play_breakout(program: Program) -> int:
-    screen: Dict[Tuple[int, int], Tile] = {}
-    score = 0
-    program[0] = 2
-    computer = IntcodeComputer(program)
-
-    ball_x = None
-    paddle_x = None
-    num_blocks = 0
-
-    step = 0
-
-    try:
-        while True:
-            if ball_x is None or paddle_x is None:
-                input_value = []
-            elif paddle_x < ball_x:
-                input_value = [1]
-            elif ball_x < paddle_x:
-                input_value = [-1]
-            else:
-                input_value = [0]
-
-            x = computer(input_value)
-            y = computer()
-
-            if x == -1 and y == 0:
-                score = computer()
-                print("score", score)
-                print("ball x", ball_x)
-                print("paddle x", paddle_x)
-                print("input value", input_value)
-                print("num_blocks", num_blocks)
-
-            else:
-                tile = Tile(computer())
-                previous_tile = screen.get((x, y), Tile.EMPTY)
-
-                if tile == Tile.BLOCK and previous_tile != Tile.BLOCK:
-                    num_blocks += 1
-                elif previous_tile == Tile.BLOCK and tile != Tile.BLOCK:
-                    num_blocks -= 1
-
-                screen[(x, y)] = tile
-
-                if tile == Tile.BALL:
-                    ball_x = x
-                elif tile == Tile.PADDLE:
-                    paddle_x = x
-
-                #                if tile in (Tile.BALL, Tile.PADDLE):
-
-    except EndProgram:
-        # return len([tile for tile in screen.values() if tile == Tile.BLOCK])
-        return score
-# print(count_blocks(program))
-
-print(play_breakout(program))
